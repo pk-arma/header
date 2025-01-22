d8f8d67db4a0d984ed7852dbc869baae28ccc37c
PANKAJ KUMAR VISHWAKARMA <92736385+VISHWAKARMAJ@users.noreply.github.com>
2025-01-22 16:08:28 +0530

    index.scss file type errofixed

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = (_, argv) => ({
       },
       exposes: {
           './header':"./src/header.js",
-          './styles': './src/index.css',
+          './styles': './src/index.scss',
         },
       shared: {
         ...deps,

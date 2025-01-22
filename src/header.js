6023a2e411e2af6c1e254cd42a8f75a2a9084aed
PANKAJ KUMAR VISHWAKARMA <92736385+VISHWAKARMAJ@users.noreply.github.com>
2025-01-22 15:57:53 +0530

    header css changes

--- src/header.js
+++ src/header.js
@@ -1,13 +1,13 @@
 const Header = ()=>{
     return(
         <>
-        <div className="bg-white">
-  <header className="absolute inset-x-0 top-0 z-50">
-    <div className="lg:hidden" role="dialog" aria-modal="true">
-      <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="bg-wight-950">
+  <header className="absolute">
+    <div className="" role="dialog" aria-modal="true">
+      <div className="fixed  bg-wight-950 w-full h-20">
         <div className="mt-6 flow-root">
-          <div className="-my-6 divide-y divide-gray-500/10">
-            <div className="space-y-2 py-6">
+          <div className="">
+            <div className="space-y-2 py-6 flex bg-slate-400">
               <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Product</a>
               <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Features</a>
               <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Marketplace</a>
